1d52fb553dcd98bf3083c312897d065016520e42
lequanglam <lqlam2004@gmail.com>
2018-08-09 11:51:35 +0700

    Update luckygames.js

--- downloads/luckygames/luckygames.js
+++ downloads/luckygames/luckygames.js
@@ -264,7 +264,7 @@ $.getScript('//canvasjs.com/assets/script/canvasjs.min.js').done(function(script
 			includeZero: false,
 		},
 		title: {
-			text: 'LuckyGames Bot - Script ID 0x01\nlequanglam.github.io',
+			text: 'LuckyGames Bot - Script ID 0x01 \nlequanglam.github.io',
 			fontSize: 16
 		},
 		data: [{
@@ -301,6 +301,8 @@ function viewLog() {
 
 function log(string) {
 	$('<p style="text-align: center; margin: 0px;">' + string + '</p>').appendTo('#log');
+	var elem = document.getElementById('log');
+  	elem.scrollTop = elem.scrollHeight;
 }
 
 function startBot() {

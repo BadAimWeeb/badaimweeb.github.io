df2b63f6b4f26312169190036af4322146756155
lequanglam <lqlam2004@gmail.com>
2018-08-14 17:57:11 +0700

    BUG FIXES - ĐM LUCKYGAMES

--- downloads/dice/luckygames/0x00.js
+++ downloads/dice/luckygames/0x00.js
@@ -345,6 +345,7 @@ function runBot() {
             data: {
                 game: 'dice',
                 coin: $('#coin').val(),
+                session: getCookie("SESSION"),
                 betAmount: betAmount,
                 prediction: prediction,
                 direction: direction,

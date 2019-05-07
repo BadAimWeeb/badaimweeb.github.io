d3dac624f77a974782c8a8f479653f23dc5e300c
lequanglam <lqlam2004@gmail.com>
2019-05-08 03:22:07 +0700

    0.1.1

--- mccolor/plugin.js
+++ mccolor/plugin.js
@@ -34,3 +34,40 @@ function c(text) {
 		return [text1, true, text1.length];
     }
 }
+
+function d(text) {
+    var color = ["a", "b", "c", "d", "e", "f"];
+    var index = 0;
+    var text1 = text.split("").map(function(x) { var data = "&" + color[index] + "&l" + x; index++; if (index == color.length) index = 0; return data; }).join("");
+	var index = 0;
+	var text2 = text.split("").map(function(x) { var data = "&" + color[index] + x; index++; if (index == color.length) index = 0; return data; }).join("");
+	if (text1.length > 256) {
+		return [text2, false, text2.length];
+    } else {
+		return [text1, true, text1.length];
+    }
+}
+
+function e(text) {
+    var color = ["a", "b", "c", "d", "e", "f"].reverse();
+    var index = 0;
+    var text1 = text.split("").map(function(x) { var data = "&" + color[index] + "&l" + x; index++; if (index == color.length) index = 0; return data; }).join("");
+	var index = 0;
+	var text2 = text.split("").map(function(x) { var data = "&" + color[index] + x; index++; if (index == color.length) index = 0; return data; }).join("");
+	if (text1.length > 256) {
+		return [text2, false, text2.length];
+    } else {
+		return [text1, true, text1.length];
+    }
+}
+
+function f(text) {
+    var color = ["a", "b", "c", "d", "e", "f"].reverse();
+    var text1 = text.split("").map(function(x) { var data = "&" + color[Math.ceil(Math.random() * color.length) - 1] + "&l" + x; return data; }).join("");
+	var text2 = text.split("").map(function(x) { var data = "&" + color[Math.ceil(Math.random() * color.length) - 1] + x; return data; }).join("");
+	if (text1.length > 256) {
+		return [text2, false, text2.length];
+    } else {
+		return [text1, true, text1.length];
+    }
+}

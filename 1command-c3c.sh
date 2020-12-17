e4841244b1853d60529b855d131aadd08207d7db
BadAimWeeb <29724654+BadAimWeeb@users.noreply.github.com>
2020-12-17 21:38:05 +0700

    Update 1command-c3c.sh

--- 1command-c3c.sh
+++ 1command-c3c.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-echo Tiến hành cài đặt tại ~/c3cbot (home directory)...
+echo "Tiến hành cài đặt tại ~/c3cbot (home directory)..."
 cd ~
 
-echo Cài đặt những thứ cần thiết để build và chạy c3cbot...
+echo "Cài đặt những thứ cần thiết để build c3cbot..."
 apt install -y nodejs-lts make clang 
 
-echo Tiến hành clone c3cbot về máy...
+echo "Tiến hành clone c3cbot về máy..."
 git clone https://github.com/c3cbot/c3c-0x ./c3cbot
 
 cd c3cbot
+echo "Tiến hành build c3cbot..."
 npm i
 npm run firstrun
 
-echo Đã cài đặt xong c3cbot! Sử dụng lệnh "cd ~/c3cbot && npm start" để tiến hành chạy bot, thay đổi cấu hình bằng lệnh "cd ~/c3cbot && npm run firstrun"
+echo "Đã cài đặt xong c3cbot! Sử dụng lệnh 'cd ~/c3cbot && npm start' để tiến hành chạy bot, thay đổi cấu hình bằng lệnh 'cd ~/c3cbot && npm run firstrun'"
